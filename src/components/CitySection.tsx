a8f39d7ad0e06d79fad908be6b54b64dcd623323
kaytran757 <kaytran757@gmail.com>
2026-03-02 11:06:36 +0700

    Updated CitySection.tsx

--- src/components/CitySection.tsx
+++ src/components/CitySection.tsx
@@ -51,7 +51,7 @@ const cityModels: Record<string, Model[]> = {
     { name: 'Mỹ Duyên', age: 26, district: 'Bắc Từ Liêm', price: 10, height: 168, weight: 55, views: 98.7, tags: ['Vui vẻ', 'Nhiệt tình'], vip: false, image: 'https://pbs.twimg.com/media/HCXfA2PakAAv1Sk?format=jpg&name=4096x4096' },
   ],
   dn: [
-    { name: 'Hoài Thương', age: 20, district: 'Hải Châu', price: 13, height: 161, weight: 48, views: 162.3, tags: ['Vui vẻ', 'Năng động'], vip: true },
+    { name: 'Hoài Thương', age: 20, district: 'Hải Châu', price: 13, height: 161, weight: 48, views: 162.3, tags: ['Vui vẻ', 'Năng động'], vip: true, image: 'https://pbs.twimg.com/media/HCXQeYlaAAAZlRl?format=jpg&name=large' },
     { name: 'Cẩm Ly', age: 22, district: 'Thanh Khê', price: 11, height: 164, weight: 52, views: 151.8, tags: ['Chiều chuộng'], vip: true },
     { name: 'Thiên Kim', age: 25, district: 'Sơn Trà', price: 15, height: 158, weight: 50, views: 144.2, tags: ['Lãng mạn', 'Tinh tế'], vip: true },
     { name: 'Bảo Ngọc', age: 19, district: 'Ngũ Hành Sơn', price: 9, height: 166, weight: 46, views: 138.6, tags: ['Nhiệt tình'], vip: false },
